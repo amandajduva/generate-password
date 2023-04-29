a1383c8c48ab299b8fd400ed8857acd53234b8b1
Amanda Duva <amanda.j.duva@gmail.com>
2023-04-29 14:03:10 -0400

    declared global variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,18 @@
 // Assignment code here
+let number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let specialCharacter = ["!", "@", "$", "%", "&", "*", "_", "+", "=", "?", "~"]; 
 
+let generatePassword = function() {
+  // prompt user input
+  let userLength = window.prompt("Enter the amount of characters you would like in your password: must be at least 8 and no more than 128.");
+
+  // If user pressed Cancel, immediately end function
+  if (!userLength) {
+    return;
+  }
+
+}
 
 // Get references to the #generate element
 let generateBtn = document.querySelector("#generate");
