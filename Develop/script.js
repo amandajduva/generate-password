c554acca8b0aac5e829fed249fef48732bebd71c
Amanda Duva <amanda.j.duva@gmail.com>
2023-04-29 20:26:23 -0400

    userLength function

--- Develop/script.js
+++ Develop/script.js
@@ -3,16 +3,25 @@ let number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 let characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 let specialCharacter = ["!", "@", "$", "%", "&", "*", "_", "+", "=", "?", "~"]; 
 
-let generatePassword = function() {
+let userLength = function() {
   // prompt user input
-  let userLength = window.prompt("Enter the amount of characters you would like in your password: must be at least 8 and no more than 128.");
+  let length = window.prompt("Enter the amount of characters you would like in your password: must be at least 8 and no more than 128.");
 
   // If user pressed Cancel, immediately end function
-  if (!userLength) {
+  if (!length) {
     return;
   }
-
+  
+  // check conditions for acceptable length
+  if (length < 8 || length > 128) {
+    window.alert("Your password must be between 8-128 chracters!");
+    length;
+  } else if (length >= 8 || length <= 128) {
+    window.alert("Great! Let's move on");
+  };
 }
+// Run length function
+userLength();
 
 // Get references to the #generate element
 let generateBtn = document.querySelector("#generate");
