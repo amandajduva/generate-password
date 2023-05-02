c4788400e18505fd31465f2ff4ea17504cafcc89
Amanda Duva <amanda.j.duva@gmail.com>
2023-05-02 10:45:36 -0400

    updated options in generatePassword function

--- Develop/script.js
+++ Develop/script.js
@@ -1,27 +1,51 @@
 // Assignment code here
-let number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-let characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-let specialCharacter = ["!", "@", "$", "%", "&", "*", "_", "+", "=", "?", "~"]; 
+const numberCharacter = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const lowercaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const specialCharacter = ["!", "@", "$", "%", "&", "*", "_", "+", "=", "?", "~"]; 
 
-let userLength = function() {
-  // prompt user input
+function generatePassword() {
+  let characterOptions = []
+
   let length = window.prompt("Enter the amount of characters you would like in your password: must be at least 8 and no more than 128.");
 
   // If user pressed Cancel, immediately end function
   if (!length) {
-    return;
+    window.alert("Your password must be between 8-128 characters!");
+    return null;
   }
   
   // check conditions for acceptable length
   if (length < 8 || length > 128) {
-    window.alert("Your password must be between 8-128 chracters!");
-    length;
+    window.alert("Your password must be between 8-128 characters!");
+    return null;
   } else if (length >= 8 || length <= 128) {
-    window.alert("Great! Let's move on");
+    window.alert("Great! Let's move on.");
   };
+
+  let lowercase = window.confirm("Do you want to include lowercase letters?");
+    if (lowercase) {
+      characterOptions = characterOptions.concat(lowercaseCharacters);
+      console.log(characterOptions);
+    }
+    
+  let uppercase = window.confirm("Do you want to include uppercase letters?");
+    if (uppercase) {
+      characterOptions = characterOptions.concat(uppercaseCharacters);
+      console.log(characterOptions);
+    }
+
+  let number = window.confirm("Do you want to include numbers?");
+    if (number) {
+      characterOptions = characterOptions.concat(numberCharacter); 
+    }
+
+  let special = window.confirm("Do you want to include special characters?");
+    if (special) {
+      characterOptions = characterOptions.concat(specialCharacter); 
+    }
 }
-// Run length function
-userLength();
+
 
 // Get references to the #generate element
 let generateBtn = document.querySelector("#generate");
